145a1b6566e3b4aa61f3213948429bfc5129000a
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-22 22:42:25 +0600

    added description

--- src/Components/FriendDetail/FriendDetail.js
+++ src/Components/FriendDetail/FriendDetail.js
@@ -11,7 +11,7 @@ const FriendDetail = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setFriend(data))
-    }, [])
+    }, [friendId])
 
     return (
         <div>
